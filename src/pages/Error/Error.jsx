00567f583e5d49812a0c3cea9dd69eeca7535f2c
GuMiao <gutianhuangfoxmail.com@wqydeMacBook-Pro.local>
2020-06-15 23:22:23 +0800

    modify error page

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import '@pages/Error/error.less'
 
 const Error = () => {
-  return <div>404</div>
+  return (
+    <div id="error">
+      <h1>The page you visit is not exist.</h1>
+      <h3>error code: 404</h3>
+    </div>
+  )
 }
 
 export default Error
