aa65647cad32a06a3ccabf9637daa1183ed1359a
GuMiao <gutianhuangfoxmail.com@wqydeMacBook-Pro.local>
2020-06-15 23:02:10 +0800

    update build

---
+++ build/precache-manifest.f4efd5b4bb5e4bfbaff1bf2e1c0fa3ba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef5966402c2ffbed2f26f752327105d2",
+    "url": "/blog/index.html"
+  },
+  {
+    "revision": "125ee05775dc9ee3d9c3",
+    "url": "/blog/static/css/main.4eea1cbe.chunk.css"
+  },
+  {
+    "revision": "6e27b980d4f7b4f5b9fd",
+    "url": "/blog/static/js/2.df41ac1c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/blog/static/js/2.df41ac1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "125ee05775dc9ee3d9c3",
+    "url": "/blog/static/js/main.e942db3a.chunk.js"
+  },
+  {
+    "revision": "18587eccc92f26eed8a3",
+    "url": "/blog/static/js/runtime-main.a5ebe4ff.js"
+  },
+  {
+    "revision": "900b7ac619a623e313a3685cc1b9c3da",
+    "url": "/blog/static/media/logo.900b7ac6.png"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.00cc67e0ad0995c60673f016fed836d1.js"
+  "/blog/precache-manifest.f4efd5b4bb5e4bfbaff1bf2e1c0fa3ba.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/blog/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
