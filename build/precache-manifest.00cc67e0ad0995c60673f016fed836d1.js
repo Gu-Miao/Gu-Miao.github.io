22b6a07c4feb4fc47675f8241ad286cac595051a
GuMiao <gutianhuangfoxmail.com@wqydeMacBook-Pro.local>
2020-06-15 22:58:30 +0800

    add build

---
+++ build/precache-manifest.00cc67e0ad0995c60673f016fed836d1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba39b843f91949d48b87bed90744e1d0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "49ea013a292656bb4d58",
+    "url": "./static/css/main.4eea1cbe.chunk.css"
+  },
+  {
+    "revision": "6e27b980d4f7b4f5b9fd",
+    "url": "./static/js/2.df41ac1c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.df41ac1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49ea013a292656bb4d58",
+    "url": "./static/js/main.e675b1bb.chunk.js"
+  },
+  {
+    "revision": "345ddccf306d8dadc0b8",
+    "url": "./static/js/runtime-main.3b935291.js"
+  },
+  {
+    "revision": "900b7ac619a623e313a3685cc1b9c3da",
+    "url": "./static/media/logo.900b7ac6.png"
+  }
+]);
