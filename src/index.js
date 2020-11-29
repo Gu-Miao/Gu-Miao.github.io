488cbf2c9482cd983439bcb6b1f07b1bf84e2697
Gu-Miao <1450206741@qq.com>
2020-11-29 23:37:11 +0800

    modify import

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import React from 'react'
+import React, { StrictMode } from 'react'
 import { render } from 'react-dom'
 import App from '@/App'
 import * as serviceWorker from '@/serviceWorker'
 
 render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>,
+  </StrictMode>,
   document.getElementById('root')
 )
 
